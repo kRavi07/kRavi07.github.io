a05abb55f066a9a4cc2078501e89e174206b1add
kRavi <48863830+yourfarm@users.noreply.github.com>
2020-08-10 21:09:08 +0530

    news box
    second commit

---
+++ news box/js/world.js
@@ -0,0 +1,34 @@
+
+
+
+fetch('https://inshortsv2.vercel.app/topic/world/40')
+            .then(function(res)
+               {
+                return (res.json());
+               }
+                )
+            .then(function(data)
+                {
+                    console.log(data);
+                  //console.log(data.data[1]);
+                  console.log(data.articles.length);
+                  var len =data.articles.length;
+                  temp ="";
+                  for(var i=0; i<len;i++)
+                  {
+                    var title = data.articles[i].title;
+                    var img =data.articles[i].image_url;
+                    var desc =data.articles[i].description;
+                    var link =data.articles[i].source_url;
+                      temp += "   <article id='featured'>";
+          
+          temp +="<a href='#'><img src="+img+" class='con_img' alt=''/></a>";
+          
+          temp +="<h1>"+title+"</h1>";
+          temp +="<p>"+desc+"</p>";
+          temp +="<a href="+link+" class='readmore'>Read more</a>";
+          temp +="</article>";
+                  }
+                  document.getElementById("contn").innerHTML=temp;
+                }
+              );
